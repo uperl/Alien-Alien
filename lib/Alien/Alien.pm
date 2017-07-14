a12d4cb7d9640cbe01ebf0939c809ff9cfa0eedc
Graham Ollis <plicease@cpan.org>
2017-07-14 11:40:22 -0400

    Initial commit

---
+++ lib/Alien/Alien.pm
@@ -0,0 +1,10 @@
+package Alien::Alien;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Find or use alien package conversion tool
+# VERSION
+
+1;
---
+++ t/alien_alien.t
@@ -0,0 +1,6 @@
+use Test2::V0;
+use Alien::Alien;
+
+ok 1, 'todo';
+
+done_testing
